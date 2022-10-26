737ea294d57223e8966cdb7651beaebc37616638
fullstop125 <hassanmoses125@gmail.com>
2022-10-26 22:37:27 +0300

    add the decode function to the message

---
+++ morse.rb
@@ -0,0 +1,42 @@
+@dictionary = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(letter)
+  @dictionary[letter]
+end
+
+def decode_word(word)
+  @word_array = word.split
+  @current_word = ''
+  @word_array.each do |letter|
+    @current_word += decode_char(letter)
+  end
+  @current_word
+end
+
